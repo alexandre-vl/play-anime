c199e36573773e1bdd2df833203affd23eec495f
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2022-07-27 00:14:21 +0200

    [FIX] Continues display players list per episode

--- src/routes/anime.js
+++ src/routes/anime.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
     let result = await Movie.find({name: req.query.name})
     if (result.length < 1) return res.status(404).send('Not found')
     console.log(result)
-    res.render('anime', {user: req.user, logged: !!req.user, anime: result[0]});
+    res.render('anime', {user: req.user, logged: !!req.user, anime: result[0], temp: {}})
 })
 
 module.exports = router;
