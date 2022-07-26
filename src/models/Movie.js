2acaf904b264085592bfc70f0b30f046efcae14b
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2022-07-26 19:11:31 +0200

    [ADD] Start using player list

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -3,15 +3,21 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({
   name: {type: String, required: true},
   img: {type: String, required: true},
-  year: {type: Number, required: true},
-  month: {type: Number, required: true},
-  day: {type: Number, required: true},
+  date: {
+    year: {type: Number, required: true},
+    month: {type: Number, required: true},
+    day: {type: Number, required: true},
+  },
   genre: {type: Array, required: true},
   rating: {type: Number, required: true},
   description: {type: String, required: true},
   type: {type: Array, required: true},
   duration: {type: String, required: true},
   director: {type: String, required: true},
+  videos: {
+    trailer: {type: String, required: true},
+    episodes: {type: Array, required: true},
+  }
 })
 
 module.exports = mongoose.model('movie', movieSchema);
