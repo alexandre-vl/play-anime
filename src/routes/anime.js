aaf5a588811de5e711907666110308d290a1d400
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2022-07-27 17:18:58 +0200

    [FIX] Next, prev buttons

--- src/routes/anime.js
+++ src/routes/anime.js
@@ -11,14 +11,11 @@ router.get('/', async (req, res) => {
 router.post('/episodes', async (req, res) => {
 
     let result = await Movie.find({name: req.body.anime_name})
-
     if (result.length < 1) return res.status(404).send('Not found')
     let anime = result[0]
     let player = anime.videos.players.find(e => e.id === parseInt(req.body.player_id)?? 1)
-
     if (!player) return res.status(404).send('Not found')
     let episode = player.episodes.find(e => e.id === parseInt(req.body.ep_id) ?? 1)
-    console.log(episode)
     if (!episode) return res.status(404).send('Not found')
     res.json({url: episode.url})
 })
